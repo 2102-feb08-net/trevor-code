f969575d693103e99804be5562113a9a24a77163
Trevor Dunbar <tdunbar123@yahoo.com>
2021-02-09 16:05:39 -0500

    Added List collections tutorial

---
+++ list-tutorial/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list_tutorial
+{
+    class Program
+    {
+        static void WorkingWithStrings()
+        {
+            var names = new List<string> { "Trevor", "Ana", "Felipe" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            Console.WriteLine();
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            Console.WriteLine($"My name is {names[0]}");
+            Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+
+            Console.WriteLine($"The list has {names.Count} people in it");
+
+            var index = names.IndexOf("Felipe");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+            }
+
+            index = names.IndexOf("Not Found");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+
+            }
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+        }
+
+        static void FibChallenge()
+        {
+            int prev = 0;
+            int curr = 1;
+            for(int i=1; i<21; i++)
+            {
+                Console.WriteLine($"Fibonacci number {i}: {curr}");
+                int temp = curr;
+                curr = curr + prev;
+                prev = temp;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //WorkingWithStrings();
+
+            var fibonacciNumbers = new List<int> {1, 1};
+
+            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+            fibonacciNumbers.Add(previous + previous2);
+
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
+
+            FibChallenge();
+        }
+    }
+}
