bcb43f10124c6d3e8f14db87d0464e87682a2292
Trevor Dunbar <tdunbar123@yahoo.com>
2021-02-10 18:17:53 -0500

    Visual Studio tutorial

---
+++ HelloVisualStudio/HelloVisualStudio/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Humanizer;
+
+namespace HelloVisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inventory App 1.0");
+            Console.Write("What item do you have? ");
+            string item = Console.ReadLine();
+            Console.Write("How many do you have? ");
+            int quantity = int.Parse(Console.ReadLine());
+
+            if (quantity != 1)
+            {
+                item = Pluralize(item);
+            }
+
+            Console.Write($"You have {quantity} {item}");
+        }
+
+        static string Pluralize(string item)
+        {
+            return item.Pluralize();
+        }
+    }
+}
